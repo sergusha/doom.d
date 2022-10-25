f45abcd272053c6a6b7754bcfceb02576739fb2f
Sergei Medvedev <sergusha@hey.com>
2022-10-25 16:03:53 +0300

    add centered mode and gitlab-ci

--- packages.el
+++ packages.el
@@ -40,6 +40,10 @@
 ;; Use `:pin' to specify a particular commit to install.
 ;(package! builtin-package :pin "1a2b3c4d5e")
 
+(package! centered-cursor-mode)
+
+(package! gitlab-ci-mode)
+(package! gitlab-ci-mode-flycheck)
 
 ;; Doom's packages are pinned to a specific commit and updated from release to
 ;; release. The `unpin!' macro allows you to unpin single packages...
