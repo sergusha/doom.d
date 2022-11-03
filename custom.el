fd08b86900f88092722f56df5a08781484ab0fe9
Sergei Medvedev <sergusha@hey.com>
2022-11-03 12:31:49 +0200

    add gruvbox

--- custom.el
+++ custom.el
@@ -4,6 +4,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("e3daa8f18440301f3e54f2093fe15f4fe951986a8628e98dcd781efbec7a46f2" "251ed7ecd97af314cd77b07359a09da12dcd97be35e3ab761d4a92d8d8cf9a71" default))
  '(package-selected-packages '(centered-cursor-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
